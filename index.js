2434baccc176d3d9a7dac004485dce928b3f17cd
madhu-Eunimart <madhu.varma@eunimart.com>
2023-05-02 19:58:56 +0530

    minor change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import dotenv from "dotenv";
 import Configure from "./configure/config.js";
-dotenv.config({path:'../node-js-sample_eunimart/.env'})
+dotenv.config({path:'../node_sample/.env'})
 class Eunimart{
     constructor(key_id,secret_key){
         this.buyer=Configure(key_id,secret_key)
