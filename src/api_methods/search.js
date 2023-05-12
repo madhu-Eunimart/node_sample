003dc36765872676e81fc973710ac83cc7c0b222
madhu-Eunimart <madhu.varma@eunimart.com>
2023-05-12 15:07:01 +0530

    changes

--- src/api_methods/search.js
+++ src/api_methods/search.js
@@ -5,17 +5,15 @@ class Order{
     this.key_id=key_id
     this.secret_key=secret_key
   }
-async Search(payload){
+async Search(payload,callback){
     if(Authentication(`${this.key_id}`,`${this.secret_key}`)){
     await axios.post(process.env.HOST_URL+"search", payload)
-      .then(function (response,callback) {
+      .then(function (response) {
         callback(response.data)
       })
       .catch(function (error) {
         console.log(error);
       });
-    }else{
-      console.log("-----")
     }
       
 }
